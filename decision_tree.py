fad2c432fccc699e0b5d8d55e7ff482aff14dcc3
Marius Hatland <marius.hatland@student.uib.no>
2022-09-19 10:29:40 +0200

    update

--- decision_tree.py
+++ decision_tree.py
@@ -2,21 +2,33 @@ import numpy as np
 import pandas as pd
 import math
 import time
+from sklearn.model_selection import train_test_split
+
 
 class DecisionTree:
     def __init__(self):
         self.tree = Node()
 
-    def learn(self, X, y, impurity_measure='entropy'):
-        build_tree(X, y, impurity_measure, self.tree)
+    def learn(self, X, y, impurity_measure='entropy', pruning=False):
+        if pruning:
+            X_train, X_prune, y_train, y_prune = train_test_split(X, y, test_size=0.25, random_state=42)
+            build_tree(X_train, y_train, impurity_measure, self.tree)
 
-    def predict(self, x):
-        return get_prediction_label(x, self.tree)
+        else:
+            build_tree(X, y, impurity_measure, self.tree)
+
+    def predict(self, X):
+        predictions = []
+        for x in X:
+            predictions.append(get_prediction_label(x, self.tree))
+        return predictions
+
 
 class Data:
     def __init__(self, split_value, split_index):
         self.split_value = split_value
         self.split_index = split_index
+
 
 class Node:
     def __init__(self, label=None, data=None):
@@ -30,18 +42,6 @@ class Node:
             return True
         return False
 
-    def add_left(self, node):
-        if self.left is None:
-            self.left = node
-        else:
-            raise Exception('Left node already has a child')
-
-    def add_right(self, node):
-        if self.right is None:
-            self.right = node
-        else:
-            raise Exception('Right node already has a child')
-
 
 def get_prediction_label(x, node):
     if node.is_leaf():
@@ -50,6 +50,12 @@ def get_prediction_label(x, node):
         return get_prediction_label(x, node.left)
     else:
         return get_prediction_label(x, node.right)
+
+
+def prune(X, y, node):
+    if node.is_leaf():
+        ...
+
 
 def build_tree(X, y, impurity_measure, node):
     unique_labels_in_y = set(y)
@@ -68,8 +74,10 @@ def build_tree(X, y, impurity_measure, node):
         node.left = Node()
         node.right = Node()
 
-        build_tree(split_info['below_split'].iloc[:, :-1], split_info['below_split'].iloc[:, -1], impurity_measure, node.left)
-        build_tree(split_info['above_split'].iloc[:, :-1], split_info['above_split'].iloc[:, -1], impurity_measure, node.right)
+        build_tree(split_info['below_split'].iloc[:, :-1], split_info['below_split'].iloc[:, -1], impurity_measure,
+                   node.left)
+        build_tree(split_info['above_split'].iloc[:, :-1], split_info['above_split'].iloc[:, -1], impurity_measure,
+                   node.right)
 
 
 def calculate_impurity(data, impurity_measure):
@@ -80,18 +88,18 @@ def calculate_impurity(data, impurity_measure):
     total_gini = 0
     for label in labels:
         number_of_rows_with_current_label = len(data.loc[data.iloc[:, -1] == label])
-        prob_current_label = number_of_rows_with_current_label/total_rows
+        prob_current_label = number_of_rows_with_current_label / total_rows
 
         if impurity_measure == "entropy":
             entropy_of_current_label = - prob_current_label * math.log2(prob_current_label)
             total_entropy += entropy_of_current_label
 
         if impurity_measure == "gini":
-            gini_of_current_label = 1 - (prob_current_label**2)
+            gini_of_current_label = 1 - (prob_current_label ** 2)
             total_gini += gini_of_current_label
 
-
     return total_entropy
+
 
 def calculate_information_gain_of_feature(data, column_index, split, impurity_measure):
     split_value = 0
@@ -108,7 +116,8 @@ def calculate_information_gain_of_feature(data, column_index, split, impurity_me
     impurity_above_split = calculate_impurity(above_split, impurity_measure=impurity_measure)
     impurity_below_split = calculate_impurity(below_split, impurity_measure=impurity_measure)
 
-    information = len(above_split)/len(data) * entropy_above_split + len(below_split)/len(data) * entropy_below_split
+    information = len(above_split) / len(data) * entropy_above_split + len(below_split) / len(
+        data) * entropy_below_split
 
     information_gain = calculate_impurity(data, "entropy") - information
 
@@ -122,10 +131,12 @@ def calculate_information_gain_of_feature(data, column_index, split, impurity_me
 
     return split_info
 
+
 def get_feature_with_highest_information_gain(data, impurity_measure, split='mean'):
     information_gains = []
     for i in range(data.shape[1] - 1):
-        information_gains.append(calculate_information_gain_of_feature(data, i, impurity_measure=impurity_measure))
+        information_gains.append(
+            calculate_information_gain_of_feature(data, i, split=split, impurity_measure=impurity_measure))
 
     obj_with_highest_information_gain = information_gains[0]
     for i in range(1, len(information_gains)):
@@ -133,6 +144,7 @@ def get_feature_with_highest_information_gain(data, impurity_measure, split='mea
             obj_with_highest_information_gain = information_gains[i]
 
     return obj_with_highest_information_gain
+
 
 def has_identical_feature_values(X):
     first_row = X.iloc[0, :]
@@ -144,9 +156,11 @@ def has_identical_feature_values(X):
 
     return True
 
+
 def get_majority_label(df):
     value_counts = df.iloc[:, -1].value_counts()
     return value_counts.sort_values(ascending=False).keys()[0]
+
 
 data = pd.read_csv('magic04.data', header=None)
 dt = DecisionTree()
